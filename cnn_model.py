a8eca4516fb14e4c9dce06d5db6cd19bc71b6d2d
anwarshaikh078 <anwarshaikh078@gmail.com>
2019-01-25 23:13:07 +0530

    First commit

---
+++ cnn_model.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+import cv2
+from collections import deque
+import numpy as np
+
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+x_train.shape
+
+image_index = 7777
+print(y_train[image_index])
+
+# Reshaping the array to 4-dims so that it can work with the Keras API
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+
+print(x_train.shape)
+
+input_shape = (28, 28, 1)
+
+# Making sure that the values are float so that we can get decimal points after division
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+
+# Normalizing the RGB codes by dividing it to the max RGB value.
+x_train /= 255
+x_test /= 255
+
+print('x_train shape:', x_train.shape)
+print('Number of images in x_train', x_train.shape[0])
+print('Number of images in x_test', x_test.shape[0])
+
+# Importing the required Keras modules containing model and layers
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
+
+# Creating a Sequential Model and adding the layers
+model = Sequential()
+model.add(Conv2D(28, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())  # Flattening the 2D arrays for fully connected layers
+model.add(Dense(128, activation=tf.nn.relu))
+model.add(Dropout(0.2))
+model.add(Dense(10, activation=tf.nn.softmax))
+
+#compiling the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(x=x_train,y=y_train,epochs=10)
+
+model.save('HRD_using_cnn_opencv.h5')
---
+++ hdr_rec.py
@@ -0,0 +1,98 @@
+import cv2
+from collections import deque
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
+from keras.models import load_model
+
+model = load_model('HRD_using_cnn_opencv.h5')
+
+def main():
+    cap = cv2.VideoCapture(0)
+    Lower_green = np.array([110, 50, 50])
+    Upper_green = np.array([130, 255, 255])
+    pts = deque(maxlen=512)
+    blackboard = np.zeros((480, 640, 3), dtype=np.uint8)
+    digit = np.zeros((200, 200, 3), dtype=np.uint8)
+    ans1 = 0
+    ans2 = 0
+    ans3 = 0
+
+    while (cap.isOpened()):
+        ab = ""
+        ret, img = cap.read()
+        img = cv2.flip(img, 1)
+        imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(imgHSV, Lower_green, Upper_green)
+        blur = cv2.medianBlur(mask, 15)
+        blur = cv2.GaussianBlur(blur, (5, 5), 0)
+        thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+        cnts = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
+        center = None
+
+        if len(cnts) >= 1:
+            cnt = max(cnts, key=cv2.contourArea)
+            if cv2.contourArea(cnt) > 250:
+                ((x, y), radius) = cv2.minEnclosingCircle(cnt)
+                cv2.circle(img, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+                cv2.circle(img, center, 5, (0, 0, 255), -1)
+                M = cv2.moments(cnt)
+                center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
+                pts.appendleft(center)
+                for i in range(1, len(pts)):
+                    if pts[i - 1] is None or pts[i] is None:
+                        continue
+                    cv2.line(blackboard, pts[i - 1], pts[i], (255, 255, 255), 10)
+                    cv2.line(img, pts[i - 1], pts[i], (0, 0, 255), 5)
+        elif len(cnts) == 0:
+            if len(pts) != []:
+                blackboard_gray = cv2.cvtColor(blackboard, cv2.COLOR_BGR2GRAY)
+                blur1 = cv2.medianBlur(blackboard_gray, 15)
+                blur1 = cv2.GaussianBlur(blur1, (5, 5), 0)
+                thresh1 = cv2.threshold(blur1, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+                blackboard_cnts = cv2.findContours(thresh1.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[1]
+                if len(blackboard_cnts) >= 1:
+                    cnt = max(blackboard_cnts, key=cv2.contourArea)
+                    print(cv2.contourArea(cnt))
+                    if cv2.contourArea(cnt) > 1800:
+                        x, y, w, h = cv2.boundingRect(cnt)
+                        digit = blackboard_gray[y:y + h, x:x + w]
+
+                        newImage = cv2.resize(digit, (28, 28))
+
+                        #newImage = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+                        #mp.imshow(newImage)
+
+                        newImage = np.array(newImage)
+
+                        newImage = newImage.flatten()
+
+                        newImage = newImage.reshape(newImage.shape[0],1)
+
+                        newImage = newImage.reshape(1,28,28,1)
+                        #newImage = newImage.reshape(newImage.shape[0],28,28,1)
+
+
+                        #print(newImage.shape)
+
+                        pred = model.predict(newImage)
+
+                        print("Result: ")
+                        ab = pred.argmax()
+                        print(pred.argmax())
+
+        x, y, w, h = 220, 100, 300, 300
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(img, "Deep Learning Model : " + str(ab), (10, 420),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.imshow("Frame", img)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            #print("Result afteR Q ")
+            pred = model.predict(newImage)
+            #print(pred.argmax())
+            break
+
+
+main()
+
